706cb49781823f9518ffd330bbea6fd7688241a2
dsal3389 <dsal3389@gmail.com>
2018-10-07 09:03:01 +0300

    Update __init__.py

--- pyupdate/__init__.py
+++ pyupdate/__init__.py
@@ -1,7 +1,8 @@
 from .UPDATE import *
+from .utils import *
 
 
 __version__='0.0.2'
 __auth__='Daniel Sonbolian'
 
-__all__=['UPDATE', 'update_files']
+__all__=['UPDATE', 'update_files', 'new_lines_count']
