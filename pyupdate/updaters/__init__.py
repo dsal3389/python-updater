fc8887bb9832c460ad59c97c0b924d0bc2ff4624
dsal3389 <dsal3389@gmail.com>
2018-10-08 20:59:08 +0300

    Update __init__.py

--- pyupdate/updaters/__init__.py
+++ pyupdate/updaters/__init__.py
@@ -1,7 +1,8 @@
 try:
     from pyupdate.updaters.update_from_file import UPDATE as update_from_file
+    from pyupdate.updaters.update_from_lines import UPDATE as update_from_lines
 except ModuleNotFoundError:
     from .update_from_file import UPDATE as update_from_file
 
 
-__all__=['update_from_file']
+__all__=['update_from_file', 'update_from_lines']
