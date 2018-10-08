77fb3de66faa42db45c9859de5054325d8209145
dsal3389 <dsal3389@gmail.com>
2018-10-08 20:50:38 +0300

    Update __init__.py

--- pyupdate/utils/__init__.py
+++ pyupdate/utils/__init__.py
@@ -1,3 +1,5 @@
 from pyupdate.utils.Genutils import *
+from pyupdate.utils._anonymous_class import ANONYMOUS
 
-__all__=['update_files', 'new_lines_count']
+__all__=['update_files', 'new_lines_count', 'compair_versions',
+         'Fversion', 'ANONYMOUSE']
