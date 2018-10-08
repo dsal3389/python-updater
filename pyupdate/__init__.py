671bd7752423e0c8fcd87f9b30cc9463968cddd8
dsal3389 <dsal3389@gmail.com>
2018-10-08 20:58:16 +0300

    import update
    cannot get utils from here anymore you need to from pyupdate.utils import *

--- pyupdate/__init__.py
+++ pyupdate/__init__.py
@@ -1,13 +1,11 @@
 from .core import *
 from .updaters import *
-from .utils import *
 
 __version__='0.1.4'
 __auth__='Daniel Sonbolian'
 
 __all__=[
 	 'update_from_file',
-         'update_files',
-         'new_lines_count',
-         'BASE_FILE'
+	 'update_from_lines',
+         'BASE_FILE',
         ]
