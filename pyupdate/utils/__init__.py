4861e2d700a0ba16544eb4a814dc05d62f81c47a
dsal3389 <dsal3389@gmail.com>
2018-10-08 20:53:52 +0300

    Update __init__.py
    typo

--- pyupdate/utils/__init__.py
+++ pyupdate/utils/__init__.py
@@ -1,5 +1,5 @@
 from pyupdate.utils.Genutils import *
 from pyupdate.utils._anonymous_class import ANONYMOUS
 
-__all__=['update_files', 'new_lines_count', 'compair_versions',
+__all__=['update_files', 'new_lines_count', 'compare_versions',
          'Fversion', 'ANONYMOUSE']
