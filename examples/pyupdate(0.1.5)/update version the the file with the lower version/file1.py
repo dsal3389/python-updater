db47b11975acb1ec8039836ef498c13c550c3c7c
dsal3389 <dsal3389@gmail.com>
2018-10-09 18:13:24 +0300

    Update file1.py

--- examples/pyupdate(0.1.5)/update version the the file with the lower version/file1.py
+++ examples/pyupdate(0.1.5)/update version the the file with the lower version/file1.py
@@ -1,4 +1,6 @@
 __version__="1.1.1"
+"""you can write the version every where you want but to overwrite this
+you need to know the line place (function will be added in the future)"""
 
 def foo():
     return 'foo'
