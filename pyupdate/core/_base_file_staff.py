3460eae6f10948d5d31c3565f77e981cef0e8518
dsal3389 <dsal3389@gmail.com>
2018-10-10 00:40:29 +0300

    Update _base_file_staff.py

--- pyupdate/core/_base_file_staff.py
+++ pyupdate/core/_base_file_staff.py
@@ -3,13 +3,13 @@ import asyncio
 class BASE_FILE:
 
     @asyncio.coroutine
-    def _write(self, file, content):
+    def _write(self, file:str, content:list) -> None:
             """overwrite to a file the given content as a list"""
             with open(self.file, 'w', encoding='utf-8')as f:
                     f.writelines(content)
 
     @asyncio.coroutine
-    def _read_file(self, file_to_read):
+    def _read_file(self, file_to_read:str) -> list:
             """read all from the given file"""
             with open(file_to_read, 'r', encoding='utf-8')as f:
                     lines = f.readlines()
