0603b88fcdfc5d7c2e07d0b57ad7a6135169bcd8
dsal3389 <dsal3389@gmail.com>
2018-10-10 21:37:33 +0300

    Update __init__.py

--- pyupdate/__init__.py
+++ pyupdate/__init__.py
@@ -1,12 +1,13 @@
 from .core import *
 from .updaters import *
 
-__version__='0.1.5'
+__version__='0.1.6'
 __state__='BETA'
 __auth__='Daniel Sonbolian'
 
 __all__=[
 	 'update_from_file',
 	 'update_from_lines',
+	 'replace_files',
          'BASE_FILE',
         ]
